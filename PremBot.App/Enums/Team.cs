13b4cfc1a669ecc19727168f6295fd1ed1515e58
Kaleb Garrett <kalebagarrett@gmail.com>
2026-08-30 11:13:24 -0400

    Replace relegated teams

--- PremBot.App/Enums/Team.cs
+++ PremBot.App/Enums/Team.cs
@@ -6,42 +6,61 @@ public enum Team
 {
     [ChoiceName("Arsenal")]
     Arsenal = 57,
+
     [ChoiceName("Aston Villa")]
     AstonVilla = 58,
+
     [ChoiceName("Bournemouth")]
     Bournemouth = 1044,
+
     [ChoiceName("Brentford")]
     Brentford = 402,
+
     [ChoiceName("Brighton")]
-    Brighton = 397, 
+    Brighton = 397,
+
     [ChoiceName("Chelsea")]
     Chelsea = 61,
+
+    [ChoiceName("Coventry City")]
+    CoventryCity = 1076,
+
     [ChoiceName("Crystal Palace")]
     CrystalPalace = 354,
+
     [ChoiceName("Everton")]
     Everton = 62,
+
     [ChoiceName("Fulham")]
     Fulham = 63,
+
+    [ChoiceName("Hull City")]
+    HullCity = 322,
+
     [ChoiceName("Ipswich Town")]
     IpswichTown = 349,
-    [ChoiceName("Leicester City")]
-    LeicesterCity = 338,
+
+    [ChoiceName("Leeds United")]
+    LeedsUnited = 341,
+
     [ChoiceName("Liverpool")]
     Liverpool = 64,
+
     [ChoiceName("Manchester City")]
     ManchesterCity = 65,
+
     [ChoiceName("Manchester United")]
     ManchesterUnited = 66,
-    [ChoiceName("Newcastle")]
-    Newcastle = 67,
+
+    [ChoiceName("Newcastle United")]
+    NewcastleUnited = 67,
+
     [ChoiceName("Nottingham Forest")]
     NottinghamForest = 351,
-    [ChoiceName("Southampton")]
-    Southampton = 340,
+
+    [ChoiceName("Sunderland")]
+    Sunderland = 71,
+
     [ChoiceName("Tottenham")]
-    Tottenham = 73,
-    [ChoiceName("West Ham")]
-    WestHam = 563,
-    [ChoiceName("Wolverhampton")]
-    Wolverhampton = 76
+    Tottenham = 73
 }
